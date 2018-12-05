7d5bc08ed8a9a61cb1f193890993892408dd93d9
Shiro Kawai <shiro@acm.org>
2018-12-04 21:46:31 -1000

    Split RUN directives to each script
    ... so that cache works better

--- samples/gauche-self-host-test/gen-dockerfile.sh
+++ samples/gauche-self-host-test/gen-dockerfile.sh
@@ -2,20 +2,21 @@
 
 VERSION=$1
 
-pakcages=
-for testfile in `awk '/^\. \/scripts\/.*/ {print $2}' scripts/self-host-test`
-do
-    packages="$packages `awk '/^#packages:/,/^#end:/ {print $2}' ./$testfile`"
-done
-
 echo $packages
 
 rm -f Dockerfile
 cat > Dockerfile <<EOF
 FROM practicalscheme/gauche:$VERSION
 RUN apt-get update
-RUN apt-get install -y autoconf automake sudo `echo $packages`
+RUN apt-get install -y autoconf automake sudo
 EOF
+
+for testfile in `awk '/^\. \/scripts\/.*/ {print $2}' scripts/self-host-test`
+do
+    packages="`awk '/^#packages:/,/^#end:/ {print $2}' ./$testfile`"
+    echo "RUN apt-get install -y `echo $packages`" >> Dockerfile
+done
+
 
 cat >> Dockerfile <<EOF
 ADD scripts /scripts
